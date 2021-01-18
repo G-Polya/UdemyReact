d8ef3b9529f2de615dedad1f0c7e781c59ad40c4
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-18 15:12:50 +0900

    assignment 1

---
+++ base-syntax--assignment-problem/src/User/UserOutput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserOutput = (props)=>{
+    return (
+        <div className="UserOutput">
+            <p>{props.name}</p>
+            <p></p>
+        </div>
+        
+    )
+};
+
+export default UserOutput;
