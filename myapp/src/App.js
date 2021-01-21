0254561a8eb83ead10e152934cdf4271f8ddc137
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-21 20:24:02 +0900

    Styled Component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import person from './Person/Person.js';
 import Person from './Person/Person.js';
+// import Radium,{StyleRoot} from 'radium';
 
 class App extends Component {
   state = {
@@ -51,11 +52,16 @@ class App extends Component {
  
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font:'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ":hover": {
+        backgroundColor:"lightgreen",
+        color:"black"
+      }
     };
 
     let persons = null;
@@ -74,25 +80,40 @@ class App extends Component {
                       />
             })
           }
-          
         </div> 
-
       )
+      style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "lightred",
+        color:"black"
+      }
     }
- 
+    
+    //let classes = ['red', 'bold'].join(' '); // "red bold"
+    let classes = []
+    if(this.state.persons.length <= 2){
+      classes.push("red"); // classes = ['red']
+    }
+
+    if(this.state.persons.length<= 1){
+      classes.push("bold"); // classes = ["red", "bold"]
+    }
+
+    
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button style={style}
-          onClick={this.togglePersonsHandler}>Toggle Person</button>
-        {persons}
-     
-        
-      </div>
+      // <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button style={style}
+            onClick={this.togglePersonsHandler}>Toggle Person</button>
+          {persons}
+        </div>
+      // </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
  
+// export default Radium(App);
 export default App;
