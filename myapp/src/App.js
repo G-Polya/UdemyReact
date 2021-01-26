08384b24079f6f5603f0ad471bd61dd326eaab34
DESKTOP-60TM4II\KimHeeSu <wlskaka4@gmail.com>
2021-01-26 20:12:12 +0900

    no message

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import person from './Person/Person.js';
+import styled from 'styled-components'
 import Person from './Person/Person.js';
 // import Radium,{StyleRoot} from 'radium';
+
+
+const StyledButton = styled.button`
+
+    background-color: green;
+    color: white;
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+    
+    &:hover: {
+      background-color: lightgreen;
+      color:black;
+    }
+
+`;
 
 class App extends Component {
   state = {
@@ -51,19 +69,7 @@ class App extends Component {
   }
  
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font:'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ":hover": {
-        backgroundColor:"lightgreen",
-        color:"black"
-      }
-    };
-
+    
     let persons = null;
     
     if(this.state.showPersons){
@@ -105,8 +111,10 @@ class App extends Component {
         <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join(' ')}>This is really working!</p>
-          <button style={style}
-            onClick={this.togglePersonsHandler}>Toggle Person</button>
+          <StyledButton onClick={this.togglePersonsHandler}>
+            Toggle Person
+          </StyledButton>
+      
           {persons}
         </div>
       // </StyleRoot>
